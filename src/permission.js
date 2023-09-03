b0de1b0e3d4564bf70878957842293bab2ffdc09
卢煌 <luhuang@jtexpress.com>
2023-09-03 20:13:42 +0800

    优化

--- src/permission.js
+++ src/permission.js
@@ -1,13 +1,8 @@
 import router from './router'
 import { ElMessage } from 'element-plus'
-// import NProgress from 'nprogress'
-// import 'nprogress/nprogress.css'
 import { getToken } from '@/utils/auth'
-// import { isHttp } from '@/utils/validate'
 import { isRelogin } from '@/utils/request'
 import useUserStore from '@/store/modules/user'
-// import useSettingsStore from '@/store/modules/settings'
-// import usePermissionStore from '@/store/modules/permission'
 
 // NProgress.configure({ showSpinner: false });
 
